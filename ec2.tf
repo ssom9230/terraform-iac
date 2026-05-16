69808c57f39f422645cee034033226e711e029b1
ssom9230 <subhankar9230@gmail.com>
2026-05-16 11:18:15 +0530

    Configure AWS provider and create EC2 instance
    Added AWS provider configuration and EC2 instance resource.

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,12 @@
-
+provider "aws" {  
+  region = "ap-south-1" 
+  access_key = "AKIA4O6CMIAASS6ELQYW"
+  secret_key = "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"
+}
+resource "aws_instance" "server1" {
+    ami = "ami-0f559c3642608c138"
+    instance_type = "t3.micro"
+    tags = {
+      Name = "server1"
+    }
+}
